48a84e11d5668ed9d065b52a8a3d8e97311851d0
raunak raj <raunakrajsrivastava7647@gmail.com>
2026-02-10 23:45:42 +0530

    mongoDb connection process

---
+++ src/index.js
@@ -0,0 +1,33 @@
+
+const dotenv = require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/connect.js";
+
+connectDB();
+
+
+
+
+
+
+
+// // can be done in this way
+// // but we used import export method to make it look more proffestional
+// // import express from "express"
+// // ;( async () => {
+// //     try{
+// //         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+// //         app.on("error",() => {
+// //             console.log("ERRRR: ",error);
+// //             throw error;
+// //         })
+// //         app.listen(process.env.PORT,() => {
+// //             console.log(`app is listening on port ${process.env.PORT}`);
+            
+// //         })
+// //     }
+// //     catch(error){
+// //         console.log("ERROR : ",error);
+        
+// //     }
+// // })()
