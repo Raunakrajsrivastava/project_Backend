a2454bb47a5d3f247a65d8a77164523fc55d91f5
raunak raj <raunakrajsrivastava7647@gmail.com>
2026-02-13 15:01:35 +0530

    MongoDB atlas connection

--- src/index.js
+++ src/index.js
@@ -1,33 +1,31 @@
 
-const dotenv = require('dotenv').config({path: './env'})
+// const dotenv = require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/connect.js";
+//import { DB_NAME } from './constants.js'; 
+// 
 
-connectDB();
+ connectDB();
 
-
-
-
-
-
-
+ 
 // // can be done in this way
-// // but we used import export method to make it look more proffestional
-// // import express from "express"
-// // ;( async () => {
-// //     try{
-// //         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-// //         app.on("error",() => {
-// //             console.log("ERRRR: ",error);
-// //             throw error;
-// //         })
-// //         app.listen(process.env.PORT,() => {
-// //             console.log(`app is listening on port ${process.env.PORT}`);
+// // // but we used import export method to make it look more proffestional
+// import express from "express"
+// import mongoose from "mongoose";
+// ;( async () => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",() => {
+//             console.log("ERRRR: ",error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT,() => {
+//             console.log(`app is listening on port ${process.env.PORT}`);
             
-// //         })
-// //     }
-// //     catch(error){
-// //         console.log("ERROR : ",error);
+//         })
+//     }
+//     catch(error){
+//         console.log("ERROR : ",error);
         
-// //     }
-// // })()
+//     }
+// })()
